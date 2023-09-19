5b0388c8bdfc16dc974cb18946270c86884bf723
MichaelVacca <michaelvacca07@gmail.com>
2023-09-19 10:53:32 -0400

    Add project files.

---
+++ MovieListApp/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieListApp.Models;
+using System.Diagnostics;
+
+namespace MovieListApp.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private MovieContext _movieContext { get; set; }
+
+		public HomeController(MovieContext ctx)
+		{
+			_movieContext = ctx;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
