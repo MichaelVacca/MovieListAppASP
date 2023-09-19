9bc067bd2f1f39259597f77736e5e3b242137859
MichaelVacca <michaelvacca07@gmail.com>
2023-09-19 11:48:52 -0400

    Added more

--- MovieListApp/Controllers/HomeController.cs
+++ MovieListApp/Controllers/HomeController.cs
@@ -9,6 +9,57 @@ namespace MovieListApp.Controllers
 		private readonly ILogger<HomeController> _logger;
 		private MovieContext _movieContext { get; set; }
 
+		[HttpGet]
+		public IActionResult Add()
+		{
+			ViewBag.Action = "Add";
+			return View("Edit", new Movie());
+		}
+
+		[HttpGet]
+		public IActionResult Edit(int id)
+		{
+			ViewBag.Action = "Edit";
+			var movie = _movieContext.Movies.Find(id);
+			return View(movie);
+		}
+
+		[HttpPost]
+		public IActionResult Edit(Movie movie)
+		{
+			if (ModelState.IsValid)
+			{
+				if (movie.MovieId == 0)
+					_movieContext.Movies.Add(movie);
+				else
+					_movieContext.Movies.Update(movie);
+				_movieContext.SaveChanges();
+				return RedirectToAction("Index", "Home");
+			}
+			else
+			{
+				ViewBag.Action =
+					(movie.MovieId == 0) ? "Add" : "Edit";
+				return View(movie);
+			}
+		}
+
+		[HttpGet]
+		public IActionResult Delete(int id)
+		{
+			var movie = _movieContext.Movies.Find(id);
+			return View(movie);
+		}
+
+		[HttpPost]
+		public IActionResult Delete(Movie movie)
+		{
+			_movieContext.Movies.Remove(movie);
+			_movieContext.SaveChanges();
+			return RedirectToAction("Index", "Home");
+		}
+
+
 		public HomeController(MovieContext ctx)
 		{
 			_movieContext = ctx;
@@ -16,7 +67,9 @@ namespace MovieListApp.Controllers
 
 		public IActionResult Index()
 		{
-			return View();
+			var movies = _movieContext.Movies.OrderBy(
+				m => m.Name).ToList();
+			return View(movies);
 		}
 
 		public IActionResult Privacy()
